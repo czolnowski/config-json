10e8158bb42917f1ca2878d429bd3236257a8ba4
Marcin Czołnowski <marcin@czolnowski.net>
2015-03-08 10:47:19 +0100

    Initial

---
+++ src/Mindweb/ConfigJson/ConfigurationJson.php
@@ -0,0 +1,25 @@
+<?php
+namespace Mindweb\ConfigJson;
+
+use JMS\Serializer;
+use Mindweb\Config;
+
+class ConfigurationJson extends Config\Configuration
+{
+    protected  function init()
+    {
+        if ($this->isInitialized()) {
+            return;
+        }
+
+        $serializer = Serializer\SerializerBuilder::create()->build();
+
+        $this->entries = $serializer->deserialize(
+            file_get_contents($this->path),
+            'array',
+            'json'
+        );
+
+        $this->setAsInitialized();
+    }
+}
